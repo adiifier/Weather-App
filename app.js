3e6f9f25639433aa8df431e7dd1c9d79c350fca4
Aditya Yadav <adityayadav2510@gmail.com>
2024-07-01 21:50:28 +0530

    bud fixes

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  "https://api.openweathermap.org/data/2.5/weather?q=";
 
+//https://api.openweathermap.org/data/2.5/weather?q=germany&appid=fe165455b310e5bf4e5e5456418f15eb&units=metric
+
+//https://api.openweathermap.org/data/2.5/weather?q=germany&appid=fe165455b310e5bf4e5e5456418f15eb
+
+//https://api.openweathermap.org/data/2.5/weather?units=metric&q=[object%20HTMLInputElement]&appid=fe165455b310e5bf4e5e5456418f15eb
 const apiKey = "fe165455b310e5bf4e5e5456418f15eb";
 const searchBox = document.querySelector(".card input");
 const btn = document.querySelector(".card button");
 const weatherImage = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-  const repsponse = await fetch(apiUrl + city + "&appid=${apiKey}");
-
-  if (Response.status == 404) {
-    document.querySelector(".error").computedStyleMap.display = "block";
-    document.querySelector(".weather").computedStyleMap.display = "none";
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`+"&units=metric");
+  let data = await response.json();
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
   } else {
-    var data = await Response.json();
+   
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°C";
@@ -32,11 +37,11 @@ async function checkWeather(city) {
       weatherImage.src = "images/Mist.png";
     }
 
-    document.querySelector(".weather").computedStyleMap.display = "block";
-    document.querySelector(".error").computedStyleMap.display = "none";
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
 }
 
 btn.addEventListener("click", () => {
-  checkWeather(searchBox);
+  checkWeather(searchBox.value);
 });
